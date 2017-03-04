051596cef3cef0340395d4b8ecf3f88ac7ed9210
abhinavtyagi <abhi007tyagi@yahoo.co.in>
2017-03-04 23:29:49 +0530

    hello NLP

---
+++ hello_nlp/hello_nlp.py
@@ -0,0 +1,6 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+text = "Hello NLP! I am Abhinav Tyagi. How are you? Hope you enjoy my NLP learning sessions :)"
+
+print(sent_tokenize(text))
+print(word_tokenize(text))
