72e757d2942538604a2a0c3b72e855a22b6f9354
abhinavtyagi <abhi007tyagi@yahoo.co.in>
2017-03-11 21:05:59 +0530

    initial commit

---
+++ JARVIS/tester.py
@@ -0,0 +1,5 @@
+import jarvis as j
+
+print(j.get_context("let's see if you can get answer to 10 divided by 5"))
+print(j.get_context("hey! can you move up the ladder?"))
+print(j.get_context("hey, how is the day today?"))
