93b7b1785dd87078ce186bd00598b0ec0ba4b123
abhinavtyagi <abhi007tyagi@yahoo.co.in>
2017-03-11 21:20:02 +0530

    added commands training data

--- JARVIS/tester.py
+++ JARVIS/tester.py
@@ -1,5 +1,5 @@
 import jarvis as j
 
-print(j.get_context("let's see if you can get answer to 10 divided by 5"))
+print(j.get_context("can you get answer to 10 divided by 5"))
 print(j.get_context("hey! can you move up the ladder?"))
-print(j.get_context("hey, how is the day today?"))
+print(j.get_context("hey, how are you today?"))
