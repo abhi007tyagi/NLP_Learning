5c3e91e18514c6599c76fc198efbeacdd2d76e2b
abhinavtyagi <abhi007tyagi@yahoo.co.in>
2017-03-11 22:19:03 +0530

    Chunked maths experssion from text

--- JARVIS/tester.py
+++ JARVIS/tester.py
@@ -1,5 +1,5 @@
 import jarvis as j
 
-print(j.get_context("can you get answer to 10 divided by 5"))
+print(j.get_context("can you get answer to 10 / 5"))
 print(j.get_context("hey! can you move up the ladder?"))
 print(j.get_context("hey, how are you today?"))
