f6279347fe7cf156221bd5896b1221c4da986670
abhinavtyagi <abhi007tyagi@yahoo.co.in>
2017-03-12 12:07:20 +0530

    added "subtracted from" and "added to" evaluation

--- hello_nlp/math_expression_finder.py
+++ hello_nlp/math_expression_finder.py
@@ -24,38 +24,40 @@ def check_word_action(exp):
     if "plus" in exp:
         exp = exp.replace("plus", "+")
         return exp
+    if "added to" in exp:
+        exp = exp.replace("added to", "+")
+        return exp
     if "minus" in exp:
         exp = exp.replace("minus", "-")
         return exp
     if "subtracted from" in exp:
-        exp = exp.replace("subtracted from", "-")
-        return exp[::-1]
+        temp = exp.split(" subtracted from ")
+        exp = temp[1] + " - " + temp[0]
+        return exp
     return exp
 
 
-text1 = "can you answer what 10 x 2 is?"
+text1 = "can you answer what 1 subtracted from 10 is?"
 text2 = "what is the value for 10 divided by 2?"
 
 tokenized1 = nltk.word_tokenize(text1);
 tags1 = nltk.pos_tag(tokenized1)
-# print(tags1)
+print(tags1)
 
 tokenized2 = nltk.word_tokenize(text2);
 tags2 = nltk.pos_tag(tokenized2)
 # print(tags2)
 
-chunkPattern = r"""Chunk: {<CD>*<JJ|VBN><IN>*<CD>}"""
+chunkPattern = r"""Chunk: {<CD>*<JJ|VBN|VBD><IN|TO>*<CD>}"""
 chunkParser = nltk.RegexpParser(chunkPattern)
 chunkedData = chunkParser.parse(tags1)
-# print("FIRST-->", chunkedData)
+print("FIRST-->", chunkedData)
 
 expression = check_word_action(get_exp(chunkedData))
-print(expression)
-print(eval(expression))
+print(expression+" = ", eval(expression))
 
 chunkedData = chunkParser.parse(tags2)
 # print("SECOND-->", chunkedData)
 
 expression = check_word_action(get_exp(chunkedData))
-print(expression)
-print(eval(expression))
+print(expression+" = ", eval(expression))
