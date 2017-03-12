268fc609847f130879d7e8651d34750f004514e2
abhinavtyagi <abhi007tyagi@yahoo.co.in>
2017-03-12 11:51:08 +0530

    first successful test for extracting and evaluating math expression from a text

--- hello_nlp/math_expression_finder.py
+++ hello_nlp/math_expression_finder.py
@@ -1,20 +1,61 @@
 import nltk
 
-text1 = "can you answer what 10 / 2 is?"
-text2 = "can you answer what 10 divided by 2 is?"
+
+def get_exp(chunked):
+    exp = ""
+    for subtree in chunked.subtrees(filter=lambda t: t.label() == 'Chunk'):
+        for l in subtree.leaves():
+            exp += " " + str(l[0])
+
+    print("get_exp ->", exp)
+    return exp
+
+
+def check_word_action(exp):
+    if "multiplied by" in exp:
+        exp = exp.replace("multiplied by", "*")
+        return exp
+    if "x" in exp:
+        exp = exp.replace("x", "*")
+        return exp
+    if "divided by" in exp:
+        exp = exp.replace("divided by", "/")
+        return exp
+    if "plus" in exp:
+        exp = exp.replace("plus", "+")
+        return exp
+    if "minus" in exp:
+        exp = exp.replace("minus", "-")
+        return exp
+    if "subtracted from" in exp:
+        exp = exp.replace("subtracted from", "-")
+        return exp[::-1]
+    return exp
+
+
+text1 = "can you answer what 10 x 2 is?"
+text2 = "what is the value for 10 divided by 2?"
 
 tokenized1 = nltk.word_tokenize(text1);
 tags1 = nltk.pos_tag(tokenized1)
-print(tags1)
+# print(tags1)
 
 tokenized2 = nltk.word_tokenize(text2);
 tags2 = nltk.pos_tag(tokenized2)
-print(tags2)
+# print(tags2)
 
 chunkPattern = r"""Chunk: {<CD>*<JJ|VBN><IN>*<CD>}"""
 chunkParser = nltk.RegexpParser(chunkPattern)
 chunkedData = chunkParser.parse(tags1)
-print("FIRST-->", chunkedData)
+# print("FIRST-->", chunkedData)
+
+expression = check_word_action(get_exp(chunkedData))
+print(expression)
+print(eval(expression))
 
 chunkedData = chunkParser.parse(tags2)
-print("SECOND-->", chunkedData)
+# print("SECOND-->", chunkedData)
+
+expression = check_word_action(get_exp(chunkedData))
+print(expression)
+print(eval(expression))
