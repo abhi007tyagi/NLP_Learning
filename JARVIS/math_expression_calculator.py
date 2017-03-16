ee5a8a5e02654e02256390580505cf7e306a1076
abhinavtyagi <abhi007tyagi@yahoo.co.in>
2017-03-16 16:43:22 +0530

    improvement to math expression calculator

--- JARVIS/math_expression_calculator.py
+++ JARVIS/math_expression_calculator.py
@@ -1,18 +1,10 @@
 import nltk
 import text2num as t2n
+from nltk.corpus import stopwords
 
 
 def get_exp(chunked):
     exp = extract_chunks(chunked, ["Chunk1", "Chunk2", "Chunk3"])
-
-    # if len(exp) == 0:
-    #     # print("second check")
-    #     exp = extract_chunks(chunked, "Chunk2")
-    #
-    # if len(exp) == 0:
-    #     # print("second check")
-    #     exp = extract_chunks(chunked, "Chunk3")
-
     print("get_exp ->", exp)
     return exp
 
@@ -32,80 +24,115 @@ def extract_chunks(chunked, tags):
     return exp
 
 
-def check_word_action(exp):
-    if "multiplied by" in exp:
-        exp = exp.replace("multiplied by", "*")
-        return exp
-    if "multiplied to" in exp:
-        exp = exp.replace("multiplied to", "*")
-        return exp
-    if "multiply" in exp:
-        exp = exp.replace("multiply ", "")
-        if "with" in exp:
-            exp = exp.replace("with", "*")
-            return exp
-        if "by" in exp:
-            exp = exp.replace("by", "*")
-            return exp
-    if "x" in exp:
-        exp = exp.replace("x", "*")
-        return exp
-    if "divided by" in exp:
-        exp = exp.replace("divided by", "/")
-        return exp
-    if "divide" in exp:
-        exp = exp.replace("divide ", "")
-        if "with" in exp:
-            exp = exp.replace("with", "/")
-            return exp
-        if "by" in exp:
-            exp = exp.replace("by", "/")
-            return exp
-    if "plus" in exp:
-        exp = exp.replace("plus", "+")
-        return exp
-    if "add" in exp:
-        exp = exp.replace("add ", "")
-        if "in" in exp:
-            exp = exp.replace("in", "+")
-            return exp
-        if "to" in exp:
-            exp = exp.replace("to", "+")
-            return exp
-    if "added to" in exp:
-        exp = exp.replace("added to", "+")
-        return exp
-    if "minus" in exp:
-        exp = exp.replace("minus", "-")
-        return exp
-    if "subtracted from" in exp:
-        temp = exp.split(" subtracted from ")
-        exp = temp[1] + " - " + temp[0]
-        return exp
-    if "subtract" in exp:
-        exp = exp.replace("subtract ", "")
-        if "from" in exp:
-            temp = exp.split(" from ")
-            exp = temp[1] + " - " + temp[0]
-            return exp
-    return exp
+def extract_direct_math_expressions(tags):
+    exp = ""
+    stack = []
+    counter = 0
+    for word in tags:
+        if "add" == word[0]:
+            stack.append(" + ")
+        elif "subtract" == word[0] or "subtracted" == word[0]:
+            stack.append(" - ")
+        elif "multiply" == word[0]:
+            stack.append(" * ")
+        elif "divide" == word[0]:
+            stack.append(" / ")
+        elif "plus" == word[0] or "+" == word[0]:
+            exp += " + "
+        elif "minus" == word[0] or "-" == word[0]:
+            exp += " - "
+        elif "multiplied" == word[0] or "*" == word[0]:
+            exp += " * "
+        elif "divided" == word[0] or "/" == word[0]:
+            exp += " / "
+
+        if word[1] == "CD":
+            exp += str(word[0])
+        if counter > 0 and len(stack) > 0:
+            exp += stack.pop()
+
+        counter += 1
+
+    # print(exp, " = "+str(eval(exp)))
+    return str(eval(exp))
+
+
+# def check_word_action(exp):
+#     if "multiplied by" in exp:
+#         exp = exp.replace("multiplied by", "*")
+#         return exp
+#     if "multiplied to" in exp:
+#         exp = exp.replace("multiplied to", "*")
+#         return exp
+#     if "multiply" in exp:
+#         exp = exp.replace("multiply ", "")
+#         if "with" in exp:
+#             exp = exp.replace("with", "*")
+#             return exp
+#         if "by" in exp:
+#             exp = exp.replace("by", "*")
+#             return exp
+#     if "x" in exp:
+#         exp = exp.replace("x", "*")
+#         return exp
+#     if "divided by" in exp:
+#         exp = exp.replace("divided by", "/")
+#         return exp
+#     if "divide" in exp:
+#         exp = exp.replace("divide ", "")
+#         if "with" in exp:
+#             exp = exp.replace("with", "/")
+#             return exp
+#         if "by" in exp:
+#             exp = exp.replace("by", "/")
+#             return exp
+#     if "plus" in exp:
+#         exp = exp.replace("plus", "+")
+#         return exp
+#     if "add" in exp:
+#         exp = exp.replace("add ", "")
+#         if "in" in exp:
+#             exp = exp.replace("in", "+")
+#             return exp
+#         if "to" in exp:
+#             exp = exp.replace("to", "+")
+#             return exp
+#     if "added to" in exp:
+#         exp = exp.replace("added to", "+")
+#         return exp
+#     if "minus" in exp:
+#         exp = exp.replace("minus", "-")
+#         return exp
+#     if "subtracted from" in exp:
+#         temp = exp.split(" subtracted from ")
+#         exp = temp[1] + " - " + temp[0]
+#         return exp
+#     if "subtract" in exp:
+#         exp = exp.replace("subtract ", "")
+#         if "from" in exp:
+#             temp = exp.split(" from ")
+#             exp = temp[1] + " - " + temp[0]
+#             return exp
+#     return exp
 
 
 def format_input(text):
     if "-" in text:
-        return text.replace("-", " - ")
-    elif "+" in text:
-        return text.replace("+", " + ")
-    elif "*" in text:
-        return text.replace("*", " * ")
-    elif "/" in text:
-        return text.replace("/", " / ")
+        text = text.replace("-", " - ")
+    if "+" in text:
+        text = text.replace("+", " + ")
+    if "*" in text:
+        text = text.replace("*", " * ")
+    if "/" in text:
+        text = text.replace("/", " / ")
+    if "calculate" in text:
+        text = text.replace("calculate", "")
     return text
 
 
-def text_to_num(text):
-    tokenized = nltk.word_tokenize(text);
-    tags = nltk.pos_tag(tokenized)
+def text_to_num(tags):
+    # tokenized = nltk.word_tokenize(text);
+    # tags = nltk.pos_tag(tokenized)
     print(tags)
     chunkPattern = r""" Chunk0: {((<NN|CD.?|RB>)<CD.?|VBD.?|VBP.?|VBN.?|NN.?|RB.?|JJ>*)<NN|CD.?>} """
     chunkParser = nltk.RegexpParser(chunkPattern)
@@ -121,31 +148,66 @@ def text_to_num(text):
         try:
             text = text.replace(exp, str(t2n.text2num(exp)))
         except Exception as e:
-            print("error", e.args)
+            print("error text2num ->", e.args)
         print(text)
     return text
 
 
 def get_math_evaluation(text):
-    text = text_to_num(text)
-    # print(text)
+    print(text)
+
+    # formatting the input text
     text = format_input(text)
-    # print(text)
-    tokenized = nltk.word_tokenize(text);
-    tags = nltk.pos_tag(tokenized)
-    print(tags)
+    print(text)
+    result = ""
 
-    chunkPattern = r""" Chunk1: {<VB|VBP|RB|VBD|JJ|NNS><CD*><IN|TO><CD*>}
-                        Chunk2: {<CD*><JJ|VB|VBP|VBN|VBD|NNS><IN|TO><CD*>}
-                        Chunk3: {<CD*><JJ|VBP|VB|VBN|VBD|NN|:><CD*>} """
-    chunkParser = nltk.RegexpParser(chunkPattern)
-    chunkedData = chunkParser.parse(tags)
-    print(chunkedData)
+    # convert word numbers to digits
+    text = text_to_num(text)
 
-    exp = get_exp(chunkedData)
-    result = "Can't extract expression!"
-    if len(exp) >= 2:
-        expression = check_word_action(exp)
-        result = str(eval(expression))
-        # return str(expression + " = " + str(eval(expression)))
+    # calculating simple expression like 10/2+2
+    try:
+        # removing any spaces
+        exp = text.replace(" ", "")
+        # print(exp)
+        result = str(eval(exp))
+    except Exception as e:
+        print("error 1 -> ", e.args)
+
+    # if result length is zero it means simple calculation failed proceed to second method
+    if len(result) == 0:
+
+        # tokenize and remove stop words
+        tokenized = nltk.word_tokenize(text)
+
+        stop_words = set(stopwords.words("english"))
+        filtered_text = [w for w in tokenized if not w in stop_words]
+        print(filtered_text)
+
+        #  tag the filtered words
+        tags = nltk.pos_tag(filtered_text)
+        print(tags)
+
+        # calculating direct math expressions like "add 10 to 6 multiplied by 7 divided by 6
+        try:
+            result = extract_direct_math_expressions(tags)
+        except Exception as e:
+            print("error 2 ->", e.args)
+
+        # if result length is zero it means second calculation failed proceed to the third rule based approach
+        if len(result) == 0:
+            # do the chunking of tags
+            chunk_pattern = r""" Chunk1: {<VB|VBP|RB|VBD|JJ|NNS><CD*><IN|TO><CD*>}
+                                Chunk2: {<CD*><JJ|VB|VBP|VBN|VBD|NNS><IN|TO><CD*>}
+                                Chunk3: {<CD*><JJ|VBP|VB|VBN|VBD|NN|:><CD*>} """
+            chunk_parser = nltk.RegexpParser(chunk_pattern)
+            chunked_data = chunk_parser.parse(tags)
+            print(chunked_data)
+
+            exp = get_exp(chunked_data)
+            result = "Can't extract expression!"
+            if len(exp) >= 2:
+                expression = check_word_action(exp)
+                result = str(eval(expression))
+                # return str(expression + " = " + str(eval(expression)))
+
     return result
--- JARVIS/tester.py
+++ JARVIS/tester.py
@@ -11,7 +11,7 @@ import text2num
 # print(j.get_context("how's today's weather"))
 # print(j.get_context("hey, add three to 4"))
 # print(j.get_context("what is three multiplied to 4"))
-# print(math_cal.get_math_evaluation("can you answer what 1898 subtracted from 103 is?"))
+# print(math_cal.get_math_evaluation("can you answer what 1898 subtracted from 100 is?"))
 # print(math_cal.get_math_evaluation("what is the value for 10 divided by 2?"))
 # print(math_cal.get_math_evaluation("multiply 7 with 5"))
 # print(math_cal.get_math_evaluation("multiply 10 by 3"))
@@ -20,12 +20,17 @@ import text2num
 # print(math_cal.get_math_evaluation("what is 5 minus 3"))
 # print(math_cal.get_math_evaluation("divide 14 by 2"))
 # print(math_cal.get_math_evaluation("answer to 10 / 5"))
-print(math_cal.get_math_evaluation("add two thousand three hundred seventy six to 100"))
-print(math_cal.get_math_evaluation("what is three hundred seventy five multiplied by 100"))
+print(math_cal.get_math_evaluation("what is the result for 7 multiplied by 6"))
+# print(math_cal.get_math_evaluation("add two thousand three hundred seventy six to 100"))
+# print(math_cal.get_math_evaluation("what is three hundred seventy five multiplied by 100"))
+# print(math_cal.get_math_evaluation("eleven million seventy five multiplied by 100"))
+print(math_cal.get_math_evaluation("multiply 1 to 2 minus 6 plus 10 multiplied by 2 divided by 4"))
+print(math_cal.get_math_evaluation("what is 10 multiplied by 6 plus 2 minus 6 plus 10 multiplied by 2 divided by 4"))
 print(math_cal.get_math_evaluation("calculate forty two thousand one hundred seventy five divided by 100"))
-print(math_cal.get_math_evaluation("eleven million seventy five multiplied by 100"))
-# text = "what is three multiplied to 4"
-
+print(math_cal.get_math_evaluation("calculate 10 / 2 - 6 + 7"))
+# text = "multiply 1 to 2 minus 6 plus 10 multiplied by 2 divided by 4"
+# text = "1/2+3-4="
+# exp = "1*2-6+10*2/4"
 # p = inflect.engine()
 # print(p.number_to_words(99))
 #
@@ -34,3 +39,4 @@ print(math_cal.get_math_evaluation("eleven million seventy five multiplied by 10
 # tokenized = nltk.word_tokenize(text)
 # tags = nltk.pos_tag(tokenized)
 # print(tags)
+# print(str(eval(exp)))
